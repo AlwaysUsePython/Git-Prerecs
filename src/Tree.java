7d6c575f8730602d23feaf63ce38144d6a8e8a01
Matthew Chang <monkeymatt2023@gmail.com>
2022-09-15 21:47:24 -0700

    Generated SHA1 in Tree

--- src/Tree.java
+++ src/Tree.java
@@ -8,4 +8,35 @@ public class Tree {
 	
 	private HashMap<String, String> map;
 
+	
+	public Tree() throws IOException {
+		
+	}
+	
+	private void saveList() throws IOException {
+		
+	}
+	
+	private static String getSHA1(String input) throws IOException, NoSuchAlgorithmException {
+		 FileInputStream fileInputStream = new FileInputStream(input);
+         MessageDigest digest = MessageDigest.getInstance("SHA-1");
+         DigestInputStream digestInputStream = new DigestInputStream(fileInputStream, digest);
+         byte[] bytes = new byte[1024];
+
+         while (digestInputStream.read(bytes) > 0);
+
+         byte[] resultByteArry = digest.digest();
+         return bytesToHexString(resultByteArry);
+	}
+	
+	public static String bytesToHexString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            int value = b & 0xFF;
+            if (value < 16)
+                sb.append("0");
+            sb.append(Integer.toHexString(value));
+        }
+        return sb.toString();
+    }
 }
