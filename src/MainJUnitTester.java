8e9c90479f265dc3f48ead1599f87568892d9817
Matthew Chang <monkeymatt2023@gmail.com>
2022-09-15 21:46:58 -0700

    Finished Tester

--- src/MainJUnitTester.java
+++ src/MainJUnitTester.java
@@ -11,11 +11,15 @@ import org.junit.jupiter.api.Test;
 
 import java.util.*;
 import java.io.*;
+import java.security.*;
 
 class MainJUnitTester {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		File folder = new File("test/objects");
+		folder.mkdir();
+		
 		Path p1 = Paths.get("something.txt");
         try {
             Files.writeString(p1, "something", StandardCharsets.ISO_8859_1);
@@ -29,21 +33,42 @@ class MainJUnitTester {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        
+        Path p3 = Paths.get("onemore.txt");
+        try {
+            Files.writeString(p3, "onemore", StandardCharsets.ISO_8859_1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
-//		File f1 = new File("index.txt");
-//		f1.delete();
-//		
-//		File f2 = new File("something.txt");
-//		f2.delete();
-//		
-//		File f3 = new File("another.txt");
-//		f3.delete();
-//		
-//		File f4 = new File("objects");
-//		f4.delete();
+		File f1 = new File("index.txt");
+		f1.delete();
+		
+		File f2 = new File("something.txt");
+		f2.delete();
+		
+		File f3 = new File("another.txt");
+		f3.delete();
+
+		File f4 = new File("onemore.txt");
+		f4.delete();
+		File f4hash = new File("test/objects/815b3ec88ab222fd849e02e19613a69ab52f5bae");
+		f4hash.delete();
+		
+		File f5 = new File("test/objects");
+		deleteDir(f5);
+	}
+	
+	static void deleteDir(File file) {
+	    File[] contents = file.listFiles();
+	    if (contents != null) {
+	        for (File f : contents)
+	            deleteDir(f);
+	    }
+	    file.delete();
 	}
 
 	@Test
@@ -54,34 +79,38 @@ class MainJUnitTester {
 		File file = new File("index.txt");
 		assertTrue(file.exists());
 		
-		Path path = Paths.get("objects");
+		Path path = Paths.get("test/objects");
 		assertTrue(Files.exists(path));
 	}
 	
 	@Test
 	void testBlob() throws Exception {
-		Blob b = new Blob("testFile.txt");
+		Blob b = new Blob("something.txt");
 		
-		File file = new File("objects/be76136a108da89559231f95ebd847b70d78e9e6");
+		File file = new File("test/objects/1af17e73721dbe0c40011b82ed4bb1a7dbe3ce29");
 		assertTrue(file.exists());
 	}
 	
 	@Test
 	void testAdd() throws Exception {
 		Index i = new Index();
-		i.add("something.txt");
+		i.initialize();
 		i.add("another.txt");
+		i.add("onemore.txt");
 		
-		File file = new File("objects/1af17e73721dbe0c40011b82ed4bb1a7dbe3ce29");
-		assertTrue(file.exists());
+		File file1 = new File("test/objects/b7c8ffb8fbc67c171328e0e8f643694e8e61b3359");
+		assertTrue(file1.exists());
+		File file2 = new File("test/objects/815b3ec88ab222fd849e02e19613a69ab52f5bae");
+		assertTrue(file2.exists());
 	}
 
 	@Test
 	void testRemove() throws IOException {
 		Index i = new Index();
-		i.remove("something.txt");
+		i.initialize();
+		i.remove("another.txt");
 		
-		File file = new File("objects/1af17e73721dbe0c40011b82ed4bb1a7dbe3ce29");
-		assertTrue(!file.exists());
+		File file = new File("test/objects/b7c8ffb8fbc67c171328e0e8f643694e8e61b335");
+		assertTrue(file.exists());
 	}
 }
---
+++ src/Tree.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+import java.io.*;
+import java.security.*;
+import java.math.BigInteger;
+
+public class Tree {
+	
+	private HashMap<String, String> map;
+
+}
